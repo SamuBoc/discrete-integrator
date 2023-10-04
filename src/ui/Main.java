6266efb971ffcd9dc52b825fec12a944813dc505
SamuBoc <123773611+SamuBoc@users.noreply.github.com>
2023-10-04 20:40:30 +0000

    folder arrangement

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -20,6 +20,19 @@ public class Main {
             option = obPpal.showMenuAndGetOption();
             obPpal.answerOption(option);
         } while (option != 0);
+
+    // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+        for (int i = 1; i <= 5; i++) {
+
+            // Press Shift+F9 to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Ctrl+F8.
+            System.out.println("i = " + i);
+        }
+
     }
 
     public void answerOption(int userOption) {
