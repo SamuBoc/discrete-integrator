c5d3feeb4bbe51cf0163c59fd7221aee2f774a09
SamuBoc <samuelandresbonilla@hotmail.com>
2023-10-14 23:38:09 -0500

    ERRORES RESUELTOS

--- src/model/Action.java
+++ src/model/Action.java
@@ -4,7 +4,6 @@ public class Action {
     public ActionType actionType;
     public Item item;
 
-
     public Action(int actionType, Item item) {
         this.actionType = ActionType.values()[actionType];
         this.item = item;
